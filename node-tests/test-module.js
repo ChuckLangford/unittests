4941c1e426644da7e1741a43b69df661b07ee192
Chuck Langford <field.email@gmail.com>
2015-10-03 15:38:22 -0400

    Added comments explaining the thought libraries and though process

--- node-tests/test-module.js
+++ node-tests/test-module.js
@@ -1,5 +1,12 @@
 'use strict';
 
+// ----------------------------------------------------------------------------
+// require('assert') - Assert is the built in Node assert library
+// require('util') - Util is built into Node.  We're using it to color output
+// require('../app/test-module') - This is the code we actually want to test
+//
+// function print(message) - Just a helper function that colors output
+// ----------------------------------------------------------------------------
 var assert = require('assert');
 var util = require('util');
 var testModule = require('../app/test-module');
@@ -8,10 +15,13 @@ function print(message) {
   return util.inspect(message, {colors:true});
 }
 
+// All of the asserts need to go into this one function.  By exporting a single
+// function, we can run all the tests we need to for our specified module
 module.exports = function() {
+  // An example test
   assert.strictEqual(
-      testModule.getOutput(),
-      'Hello!',
-      print('test-module getOutput() did not match expected \'Hello!\'')
+      testModule.getOutput(), // actual
+      'Hello!', // expected
+      print('test-module getOutput() did not match expected \'Hello!\'') //error
   );
 };
--- node-tests/tests.js
+++ node-tests/tests.js
@@ -1,3 +1,8 @@
 'use strict';
 
+// ----------------------------------------------------------------------------
+// The idea behind the tests.js file is to simply run each of the individual
+// module tests that are in the same directory.  Each module unit test needs to
+// export a single function.  That function simply runs all the asserts.
+// ----------------------------------------------------------------------------
 require('./test-module')();
